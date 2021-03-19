db76cdd7eb4459aea2da845f017470b21e4dc6b7
Heldy JULIANA <SESA440895@se.com>
2021-03-19 23:07:50 +0700

    Tugas 1

---
+++ Tugas-1/Soal-1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+Nama=input("Nama saya:")
+Umur=input("Umur saya:")
+Tinggi=input("Tinggi saya:")
+print("Nama saya "+ Nama + ", umur saya " + Umur + " tahun dan tinggi saya " + Tinggi + " cm.")
+
---
+++ Tugas-1/Soal-2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+r=float(input("jari-jari = "))
+phi=float(22/7)
+luas_lingkaran= format(phi*r**2, ".2f")
+print("Luas lingkaran dengan jari-jari {0} cm adalah {1} cm\u00b2".format(r, luas_lingkaran))
+
